152262a26a75fc2ffa33b21b9bb42088760f57c1
ProfMobius <unknown>
2014-03-19 18:45:18 +0100

    More clean up

--- mcp/mobius/opis/network/Packets.java
+++ mcp/mobius/opis/network/Packets.java
@@ -2,14 +2,14 @@ package mcp.mobius.opis.network;
 
 public class Packets {
 	
-	public static byte LOADED_CHUNKS       = 0x01;	// S => C
-	public static byte REQ_CHUNKS          = 0x02;	// C => S //Not used right now
-	public static byte TICKETS             = 0x03;	// S => C
-	public static byte CHUNKS              = 0x04;	// S => C
-	public static byte MEANTIME            = 0x05;	// S => C
-	public static byte TPS                    = 0x06;
-	public static byte MODMEANTIME            = 0x07;  // S => C	
-	public static byte CLIENT_CMD             = 0x08;
+	public static byte LOADED_CHUNKS = 0x01;	// S => C
+	public static byte REQ_CHUNKS    = 0x02;	// C => S //Not used right now
+	public static byte TICKETS       = 0x03;	// S => C
+	public static byte CHUNKS        = 0x04;	// S => C
+	public static byte MEANTIME      = 0x05;	// S => C
+	public static byte TPS           = 0x06;
+	public static byte MODMEANTIME   = 0x07;  // S => C	
+	public static byte CLIENT_CMD    = 0x08;
 	public static byte DATA_OVERLAY_CHUNK_ENTITIES = 0x09;
 	public static byte DATA_LIST_GENERAL           = 0x10;
 	public static byte REQ_DATA                    = 0x11; // C => S
