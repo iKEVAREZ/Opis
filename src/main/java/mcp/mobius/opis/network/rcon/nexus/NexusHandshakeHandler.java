c093287b5cbec8db674984c8016508ce28f68a41
ProfMobius <unknown>
2014-10-24 21:29:00 +0200

    We moved the modification of the pipeline inside the ChannelActive, post handshake success. This correct the missing auth packets at the beginning of the transaction.

--- src/main/java/mcp/mobius/opis/network/rcon/nexus/NexusHandshakeHandler.java
+++ src/main/java/mcp/mobius/opis/network/rcon/nexus/NexusHandshakeHandler.java
@@ -4,20 +4,41 @@ import mcp.mobius.opis.modOpis;
 import io.nettyopis.buffer.ByteBuf;
 import io.nettyopis.channel.ChannelHandlerContext;
 import io.nettyopis.channel.ChannelInboundHandlerAdapter;
+import io.nettyopis.handler.codec.compression.JdkZlibDecoder;
+import io.nettyopis.handler.codec.compression.JdkZlibEncoder;
 import io.nettyopis.handler.ssl.SslHandler;
+import io.nettyopis.util.ReferenceCountUtil;
 import io.nettyopis.util.concurrent.Future;
 import io.nettyopis.util.concurrent.GenericFutureListener;
 import io.nettyopis.channel.Channel;
 
 public class NexusHandshakeHandler extends ChannelInboundHandlerAdapter {
     
-	@Override
+    @Override
     public void channelActive(final ChannelHandlerContext ctx) throws Exception {
-		modOpis.log.info("SSL encryption activated.");
-        ctx.pipeline().remove(NexusHandshakeDecoder.class);
-        ctx.pipeline().remove(NexusHandshakeHandler.class);
-        ctx.pipeline().addLast(new NexusMsgDecoder());
-        ctx.pipeline().addLast(new NexusInboundHandler());
+        ctx.pipeline().get(SslHandler.class).handshakeFuture().addListener(
+
+                new GenericFutureListener<Future<Channel>>() {
+                    @Override
+                    public void operationComplete(Future<Channel> future) throws Exception {
+                    	modOpis.log.info("SSL encryption activated.");
+                        ctx.pipeline().remove(NexusHandshakeDecoder.class);
+                        ctx.pipeline().remove(NexusHandshakeHandler.class);
+            			ctx.pipeline().addLast(new JdkZlibDecoder());
+            			ctx.pipeline().addLast(new JdkZlibEncoder());                        
+                        ctx.pipeline().addLast(new NexusMsgDecoder());
+                        ctx.pipeline().addLast(new NexusInboundHandler());
+                    }
+                });
+    }	
+	
+	@Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try{
+        	modOpis.log.warn("You shouldn't see this. This handler is never supposed to be active !");
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
     }
 
     @Override
